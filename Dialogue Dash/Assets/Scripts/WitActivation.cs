314e1b8d0ef06b1aba6c37136eae1cad5039430e
danielcasto <32224738+danielcasto@users.noreply.github.com>
2021-11-03 16:11:17 -0400

    added mic activation

---
+++ Dialogue Dash/Assets/Scripts/WitActivation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitActivation : MonoBehaviour
+{
+    [SerializeField] private Wit wit;
+
+    private void OnValidate()
+    {
+        if (!wit) wit = GetComponent<Wit>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            wit.Activate();
+        }
+    }
+}
